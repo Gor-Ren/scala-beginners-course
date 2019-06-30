0b9f82a1f69dab36d3d8a0890b56db94ba8e837a
Gordon <gor.rennie@gmail.com>
2019-06-30 12:37:23 +0100

    Part 2.8: Complete exercise
    Signed-off-by: Gordon <gor.rennie@gmail.com>

---
+++ src/lectures/part2oop/MyList.scala
@@ -0,0 +1,61 @@
+package lectures.part2oop
+
+import java.util.NoSuchElementException
+
+/**
+  * An immutable, singly-linked list of integers.
+  */
+abstract class MyList {
+
+  /*
+   * Exercise: implement class with methods:
+   *   head = first element
+   *   tail = rest of the list
+   *   isEmpty = true is list empty, else false
+   *   add(Int) => new list with element appended to end
+   *   toString => a string representation of the list
+   */
+  def head: Int
+  def tail: MyList
+  def isEmpty: Boolean
+  def add(element: Int): MyList
+  def elementsToString: String
+  override def toString: String = s"[$elementsToString]"
+}
+
+object Empty extends MyList {  // we make it an object since it can be a singleton
+  override def head: Int = throw new NoSuchElementException("List is empty")
+
+  override def tail: MyList = throw new NoSuchElementException("List is empty")
+
+  override def isEmpty = true
+
+  override def add(element: Int) = new Cons(element)
+
+  override def elementsToString: String = ""
+}
+
+class Cons(override val head: Int, override val tail: MyList = Empty) extends MyList {
+  // something cool happened: overrode "value methods" head and tail with vals in signature
+
+  override def isEmpty = false
+
+  override def add(element: Int) = new Cons(element, this)
+
+  override def elementsToString: String = {
+    if (tail.isEmpty) head.toString
+    else head.toString + " " + tail.elementsToString
+  }
+}
+
+object MyListTest extends App {
+  val singletonList = new Cons(1)
+  println(singletonList.head)
+
+  val list = new Cons(2, singletonList)
+  println(list.head)
+  println(list.add(3).head)
+  println(list.tail)
+
+  println(list.add(3).add(4).add(0))
+}
